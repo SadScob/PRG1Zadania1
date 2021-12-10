72fd2bbff9a6f2b0a0b54acd346fe0faf015ce25
s24269 <92214376+SadScob@users.noreply.github.com>
2021-12-10 13:46:23 +0100

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name("Alex");
+    double waga = 66;
+    int wzrost = 185;
+
+    cout << name << "\n" << endl;
+    cout << waga << "\n"<< endl;
+    cout << wzrost << "\n"<< endl;
+    return 0;
+}
